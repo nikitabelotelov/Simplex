ab437ed434b365a5b8d736dbc208cee46e65b8ba
nikitabelotelov <nikitabelotelov@mail.ru>
2017-01-06 19:51:57 +0300

    Task creation

---
+++ Simplex/MatrixTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mehroz;
+
+namespace Simplex
+{
+    class MatrixTask
+    {
+        Fraction[,] Matrix;
+        int vars;
+        int conds;
+        public MatrixTask(int vars, int conds)
+        {
+            Matrix = new Fraction[conds, vars];
+        }
+        public Fraction this[int i, int j]
+        {
+            get
+            {
+                return Matrix[i, j];
+            }
+            set
+            {
+                Matrix[i, j] = value;
+            }
+        }
+    }
+}
--- Simplex/TaskCreationWindow.xaml.cs
+++ Simplex/TaskCreationWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
 namespace Simplex
 {
     /// <summary>
@@ -20,28 +21,16 @@ namespace Simplex
     /// 
     public partial class TaskCreationWindow : Window
     {
-        private int varNum;
-        private int condNum;
-
+        private int VarNum;
+        private int CondNum;
+        private const int CellSize = 30;
         public TaskCreationWindow()
         {
             InitializeComponent();
         }
 
-        private void matrixInfoChanged(object sender, TextChangedEventArgs e)
+        private bool SetMatrixInfo()
         {
-            if (systemInfo == null)
-                return;
-            if(!checkMatrixInfo())
-                systemInfo.Text = "Количество переменных и условий должно быть числом от 1 до 16.";
-            else
-                systemInfo.Text = "";
-        }
-
-        private bool checkMatrixInfo()
-        {
-            if (systemInfo == null)
-                return true;
             if (condNumTB.Text == "" || varNumTB.Text == "")
                 return false;
             int vars, conds;
@@ -52,20 +41,39 @@ namespace Simplex
             else if (vars < 1 || vars > 16 || conds < 1 || conds > 16)
                 return false;
             else
-                return true;
-
-        }
-
-        private void matrixInputGrid_Initialized(object sender, EventArgs e)
-        {
-            List<TextBox> inputMatrixTBList = new List<TextBox>(64);
-            for (int i = 0; i < 256; i++)
             {
-                ;
+                VarNum = vars;
+                CondNum = conds;
+                return true;
             }
         }
 
         private void SetButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!SetMatrixInfo())
+                MessageBox.Show("Количество переменных и условий должны быть числами от 1 до 16.");
+            else
+            {
+                MatrixTask matrixTask = new MatrixTask(VarNum, CondNum);
+                TextBox[] matrixTBs = new TextBox[VarNum * CondNum];
+                MatrixField.Children.Clear();
+                MatrixField.Margin = new Thickness(10, 76, 0, 0);
+                MatrixField.Width = VarNum * CellSize;
+                MatrixField.Height = CondNum * CellSize;
+                for (int i = 0; i < matrixTBs.Length; i++)
+                {
+                    matrixTBs[i] = new TextBox();
+                    matrixTBs[i].Text = 0.ToString();
+                    matrixTBs[i].Width = CellSize;
+                    matrixTBs[i].HorizontalAlignment = HorizontalAlignment.Left;
+                    //matrixTBs[i].Height = CellSize;
+                    matrixTBs[i].VerticalAlignment = VerticalAlignment.Center;
+                    matrixTBs[i].Margin = new Thickness((i % VarNum) * CellSize, (i / VarNum) * CellSize, 0, 0);
+                    MatrixField.Children.Add(matrixTBs[i]);
+                }
+            }
+        }
+        private void GenMatrixField()
         {
 
         }
